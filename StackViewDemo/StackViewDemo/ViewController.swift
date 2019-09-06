871281ed8f387917f3f0401f7819750277dc3945
siddharthkaushal786 <52787619+siddharthkaushal786@users.noreply.github.com>
2019-09-06 18:20:10 +0530

    Add files via upload

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by trainee on 01/08/19.
+//  Copyright © 2019 trainee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
