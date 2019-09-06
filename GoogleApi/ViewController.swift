907d830386887caf03671c95c34725eaa57b7769
siddharthkaushal786 <52787619+siddharthkaushal786@users.noreply.github.com>
2019-09-06 18:32:57 +0530

    Add files via upload

---
+++ GoogleApi/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  GoogleApi
+//
+//  Created by trainee on 31/07/19.
+//  Copyright © 2019 trainee. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import CoreLocation
+import MapKit
+import GooglePlacesSearchController
+import GooglePlaces
+class ViewController: UIViewController{
+   
+    
+  
+    
+  //  let googleSearchPleacesKey = "AIzaSyADvusy5scNHUVhtiRUGN2bZZZxSagNSTw"
+    
+//    lazy var placesSearchController: GooglePlacesSearchController = {
+//        let controller = GooglePlacesSearchController(delegate: self,
+//                                                      apiKey: GoogleMapsAPIServerKey,
+//                                                      placeType: .address)
+//
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+        
+    }
+    
+ 
+        
+        //let url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyADvusy5scNHUVhtiRUGN2bZZZxSagNSTw"
+        
+       
+    @IBAction func btnSearch(_ sender: Any) {
+       
+    }
+    
+
+}
+
--- Podfile
+++ Podfile
@@ -1,10 +1,14 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'SideMenu' do
+target 'GoogleApi' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for SideMenu
-pod 'SideMenu'
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+pod 'GooglePlacesSearchController'
+
+pod 'GooglePlaces'
+
 end
