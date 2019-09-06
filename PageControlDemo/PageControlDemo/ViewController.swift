f5759f6035067516e8afe5d36109565ce97013cb
siddharthkaushal786 <52787619+siddharthkaushal786@users.noreply.github.com>
2019-09-06 18:22:35 +0530

    Add files via upload

---
+++ PageControlDemo/PageControlDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageControlDemo
+//
+//  Created by trainee on 13/08/19.
+//  Copyright © 2019 trainee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
